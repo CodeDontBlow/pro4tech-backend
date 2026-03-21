dab77f42a887a49f280105e9e8477fff1bcfd628
Ramon <ramonads.42@gmail.com>
2026-03-21 12:44:24 -0300

    refactor: mover qr-code para módulo dedicado e traduzir para inglês

--- src/companies/companies.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { GeradorQrService } from './geradorQR.service';
-
-@Module({
-  providers: [GeradorQrService],
-  exports: [GeradorQrService],
-})
-export class CompaniesModule {}
