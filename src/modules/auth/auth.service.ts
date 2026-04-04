c4a52498b87aca7d3d71ec23fa35259821dc6370
Victor Godoy <victor.hgodoy@icloud.com>
2026-04-04 20:53:50 -0300

    fix: remocao campo role nao necessario

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -35,8 +35,7 @@ export class AuthService {
     };
 
     return {
-      access_token: await this.jwtService.signAsync(payload),
-      role: user.role
+      access_token: await this.jwtService.signAsync(payload)
     };
   }
 }
