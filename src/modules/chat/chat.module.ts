35f47ec3283186a7dd6448798b09d7c0aa28397b
Raphaela <bmraphaela@gmail.com>
2026-05-03 18:51:53 -0300

    feat: ajustando encerramento do chamado

--- src/modules/chat/chat.module.ts
+++ src/modules/chat/chat.module.ts
@@ -1,22 +1,22 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from '@modules/auth/auth.module';
 import { TicketModule } from '@modules/ticket/ticket.module';
 import { ChatGateway } from './chat.gateway';
 import { ChatService } from './chat.service';
 import { ChatMessage, ChatMessageSchema } from './schemas/chat-message.schema';
-
 @Module({
   imports: [
     AuthModule,
-    TicketModule,
     MongooseModule.forFeature([
       {
         name: ChatMessage.name,
         schema: ChatMessageSchema,
       },
     ]),
+    forwardRef(() => TicketModule),
   ],
   providers: [ChatGateway, ChatService],
+  exports: [ChatGateway, ChatService],
 })
-export class ChatModule {}
+export class ChatModule { }
