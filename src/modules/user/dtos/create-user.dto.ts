d71b18211f95eb222ac8b52f18c7abae19fa0739
EstupendoG <rafagonpin317@gmail.com>
2026-05-31 19:48:49 -0300

    Ajustes para editar Agent

--- src/modules/user/dtos/create-user.dto.ts
+++ src/modules/user/dtos/create-user.dto.ts
@@ -1,5 +1,6 @@
 import { ApiPropertyOptional, IntersectionType } from '@nestjs/swagger';
-import { IsOptional, IsUUID } from 'class-validator';
+import { IsEnum, IsOptional, IsUUID } from 'class-validator';
+import { SupportLevel } from 'generated/prisma/client';
 import { BaseUserDto } from './base-user.dto';
 
 export class AdditionalCreateFields {
@@ -11,6 +12,25 @@ export class AdditionalCreateFields {
   @IsUUID('7', { message: 'O ID da empresa deve ser um código UUID válido' })
   @IsOptional()
   companyId?: string;
+
+  @ApiPropertyOptional({
+    example: 'LEVEL_1',
+    description: 'Nível de suporte quando o usuário for um agente',
+    enum: SupportLevel,
+  })
+  @IsEnum(SupportLevel, {
+    message: 'O Nível de Suporte deve ser um dos seguintes valores: LEVEL_1, LEVEL_2, or LEVEL_3',
+  })
+  @IsOptional()
+  supportLevel?: SupportLevel;
+
+  @ApiPropertyOptional({
+    example: '550e8400-e29b-41d4-a716-446655440000',
+    description: 'ID do grupo de suporte para atribuição inicial do agente',
+  })
+  @IsUUID('7', { message: 'O ID do grupo deve ser um código UUID válido' })
+  @IsOptional()
+  supportGroupId?: string;
 }
 
 export class CreateUserDto extends IntersectionType(
